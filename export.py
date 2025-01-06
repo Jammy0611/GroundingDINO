dea15770331dad3216a547ea4302d57817070575
szsteven008 <97944818+szsteven008@users.noreply.github.com>
2025-01-06 16:12:54 +0800

    Add files via upload
    将onnx分成两个阶段,encode/decode

--- export.py
+++ export.py
@@ -12,37 +12,57 @@ import onnxruntime as ort
 from groundingdino.util.inference import load_model, annotate
 import groundingdino.datasets.transforms as T
 from groundingdino.util.utils import get_phrases_from_posmap
-from groundingdino.models.GroundingDINO.bertwarper import generate_masks_with_special_tokens_and_transfer_map
+from groundingdino.models.GroundingDINO.bertwarper import generate_masks_with_special_tokens
 
-class Model(torch.nn.Module):
-    def __init__(
-        self,
-        model_config_path: str,
-        model_checkpoint_path: str,
-        device: str = "cuda"
-    ):
+def preprocess_caption(caption: str) -> str:
+    result = caption.lower().strip()
+    if result.endswith("."):
+        return result
+    return result + "."
+
+class Encoder(torch.nn.Module):
+    def __init__(self, model):
         super().__init__()
-        self.model = load_model(
-            model_config_path=model_config_path,
-            model_checkpoint_path=model_checkpoint_path,
-            device=device
-        ).to(device)
-        self.tokenizer = self.model.tokenizer
-        self.specical_tokens = self.model.specical_tokens
-        self.max_text_len = self.model.max_text_len
+        self.tokenizer = model.tokenizer
+        self.bert = model.bert
+        self.specical_tokens = model.specical_tokens
 
-#    def forward(self, samples: NestedTensor, targets: List = None, **kw):
+    def forward(self, 
+                input_ids: torch.Tensor, 
+                token_type_ids: torch.Tensor, 
+                text_self_attention_masks: torch.Tensor, 
+                position_ids: torch.Tensor):
+        # extract text embeddings
+        tokenized_for_encoder = {}
+        tokenized_for_encoder["input_ids"] = input_ids
+        tokenized_for_encoder["token_type_ids"] = token_type_ids
+        tokenized_for_encoder["attention_mask"] = text_self_attention_masks.type(torch.bool)
+        tokenized_for_encoder["position_ids"] = position_ids
+
+        bert_output = self.bert(**tokenized_for_encoder)  # bs, 195, 768
+
+        return bert_output["last_hidden_state"]
+
+class Decoder(torch.nn.Module):
+    def __init__(self, model):
+        super().__init__()
+        self.model = model
+        self.tokenizer = model.tokenizer
+        self.specical_tokens = model.specical_tokens
+
     def forward(self, 
                 image: torch.Tensor, 
-                input_ids: torch.Tensor, 
+                last_hidden_state: torch.Tensor, 
                 attention_mask: torch.Tensor, 
-                token_type_ids: torch.Tensor, 
                 position_ids: torch.Tensor, 
                 text_self_attention_masks: torch.Tensor, 
                 box_threshold: torch.Tensor, 
-                text_threshold: torch.Tensor,
-                **kw):
-        outputs = self.model(image, input_ids, attention_mask, token_type_ids, position_ids, text_self_attention_masks)
+                text_threshold: torch.Tensor):
+        outputs = self.model(image, 
+                             last_hidden_state, 
+                             attention_mask, 
+                             position_ids, 
+                             text_self_attention_masks.type(torch.bool))
         prediction_logits = outputs["pred_logits"].sigmoid().squeeze(0)
         prediction_boxes = outputs["pred_boxes"].squeeze(0)
 
@@ -51,7 +71,120 @@ class Model(torch.nn.Module):
         prediction_input_ids_mask = prediction_logits > text_threshold
         prediction_boxes = prediction_boxes[mask]
 
-        return prediction_logits.max(dim=1)[0].unsqueeze(0), prediction_boxes.unsqueeze(0), prediction_input_ids_mask.unsqueeze(0)
+        return (prediction_logits.max(dim=1)[0].unsqueeze(0), 
+                prediction_boxes.unsqueeze(0), 
+                prediction_input_ids_mask.unsqueeze(0))
+
+def export_encoder(model, output):
+    onnx_file = output + "/" + "gdino.encoder.onnx"
+    caption = preprocess_caption("watermark")
+    tokenized = model.tokenizer(caption, padding="longest", return_tensors="pt")
+
+    (
+        text_self_attention_masks, 
+        position_ids
+    ) = generate_masks_with_special_tokens(tokenized, model.specical_tokens, model.tokenizer)
+
+    torch.onnx.export(
+        model,
+        args = (
+            tokenized["input_ids"].type(torch.int).to("cpu"), 
+            tokenized["token_type_ids"].type(torch.int).to("cpu"),
+            text_self_attention_masks.type(torch.uint8).to("cpu"),
+            position_ids.type(torch.int).to("cpu"),
+        ), 
+        f = onnx_file,
+        input_names = [ "input_ids", "token_type_ids", "text_self_attention_masks", "position_ids" ],
+        output_names = [ "last_hidden_state" ], 
+        opset_version = 17, 
+        export_params = True, 
+        do_constant_folding = True,
+        dynamic_axes = {
+            "input_ids": { 1: "token_num" },
+            "token_type_ids": { 1: "token_num" },
+            "text_self_attention_masks": { 1: "token_num", 2: "token_num" },
+            "position_ids": { 1: "token_num" },
+            "last_hidden_state": { 1: "token_num" }
+        },
+    )
+
+    print("export gdino.encoder.onnx ok!")
+
+    onnx_model = onnx.load(onnx_file)
+    onnx.checker.check_model(onnx_model)
+    print("check gdino.encoder.onnx ok!")
+
+def export_decoder(model, output, encoder):
+    onnx_file = output + "/" + "gdino.decoder.onnx"
+    caption = preprocess_caption("watermark")
+
+    tokenized, last_hidden_state = inference_encoder_onnx(encoder, output, caption)
+
+    box_threshold = torch.tensor(0.35, dtype=torch.float32)
+    text_threshold = torch.tensor(0.25, dtype=torch.float32)
+
+    torch.onnx.export(
+        model,
+        args = (
+            torch.rand(1, 3, 800, 800).type(torch.float32).to("cpu"), 
+            last_hidden_state, 
+            tokenized["attention_mask"].type(torch.uint8).to("cpu"), 
+            tokenized["position_ids"].type(torch.int).to("cpu"), 
+            tokenized["text_self_attention_masks"].type(torch.uint8).to("cpu"), 
+            box_threshold, 
+            text_threshold),
+        f = onnx_file,
+        input_names = [ "image", "last_hidden_state", "attention_mask", 
+                        "position_ids", "text_self_attention_masks", 
+                        "box_threshold", "text_threshold" ],
+        output_names = [ "logits", "boxes", "masks" ], 
+        opset_version = 17, 
+        export_params = True, 
+        do_constant_folding = True,
+        dynamic_axes = {
+            "last_hidden_state": { 1: "token_num" },
+            "attention_mask": { 1: "token_num" },
+            "position_ids": { 1: "token_num" },
+            "text_self_attention_masks": { 1: "token_num", 2: "token_num" }
+        },
+    )
+
+    print("export gdino.decoder.onnx ok!")
+
+    onnx_model = onnx.load(onnx_file)
+    onnx.checker.check_model(onnx_model)
+    print("check gdino.decoder.onnx ok!")
+
+def inference_encoder_onnx(model, output, caption: str = None):
+    onnx_file = output + "/" + "gdino.encoder.onnx"
+    session = ort.InferenceSession(onnx_file)
+
+    if caption:
+        proc_caption = preprocess_caption(caption)
+    else:
+        proc_caption = preprocess_caption("watermark. cat. dog")
+    tokenized = model.tokenizer(proc_caption, padding="longest", return_tensors="pt")
+
+    (
+        text_self_attention_masks, 
+        position_ids
+    ) = generate_masks_with_special_tokens(tokenized, model.specical_tokens, model.tokenizer)
+
+    tokenized["text_self_attention_masks"] = text_self_attention_masks
+    tokenized["position_ids"] = position_ids
+
+    outputs = session.run(None, {
+        "input_ids": tokenized["input_ids"].numpy().astype(np.int32), 
+        "token_type_ids": tokenized["token_type_ids"].numpy().astype(np.int32),
+        "text_self_attention_masks": tokenized["text_self_attention_masks"].numpy().astype(np.uint8),
+        "position_ids": tokenized["position_ids"].numpy().astype(np.int32)
+    })
+
+    if caption == None:
+        print(outputs)
+
+    last_hidden_state = torch.from_numpy(outputs[0]).type(torch.float32)
+    return tokenized, last_hidden_state
 
 def preprocess_image(image_bgr: np.ndarray) -> torch.Tensor:
     image_bgr = cv2.resize(image_bgr, (800, 800))
@@ -66,148 +199,36 @@ def preprocess_image(image_bgr: np.ndarray) -> torch.Tensor:
     image_transformed, _ = transform(image_pillow, None)
     return image_transformed
 
-def preprocess_caption(caption: str) -> str:
-    result = caption.lower().strip()
-    if result.endswith("."):
-        return result
-    return result + "."
-
-def export_onnx(model, output_dir):
-    onnx_file = output_dir + "/" + "gdino.onnx"
-    caption = preprocess_caption("watermark")
-    tokenized = model.tokenizer(caption, padding="longest", return_tensors="pt")
-    box_threshold = torch.tensor(0.35, dtype=torch.float32)
-    text_threshold = torch.tensor(0.25, dtype=torch.float32)
-
-    specical_tokens = model.specical_tokens
-    (
-        text_self_attention_masks,
-        position_ids,
-        _,
-    ) = generate_masks_with_special_tokens_and_transfer_map(
-        tokenized, specical_tokens, model.tokenizer
-    )
-
-    torch.onnx.export(
-        model,
-        args = (
-            torch.rand(1, 3, 800, 800).type(torch.float32).to("cpu"), 
-            tokenized["input_ids"].type(torch.int).to("cpu"), 
-            tokenized["attention_mask"].type(torch.uint8).to("cpu"), 
-            tokenized["token_type_ids"].type(torch.int).to("cpu"), 
-            position_ids.type(torch.int).to("cpu"), 
-            text_self_attention_masks.type(torch.bool).to("cpu"), 
-            box_threshold, 
-            text_threshold),
-        f = onnx_file,
-        input_names = [ "image", "input_ids", "attention_mask", "token_type_ids", "position_ids", "text_self_attention_masks", "box_threshold", "text_threshold" ],
-        output_names = [ "logits", "boxes", "masks" ], 
-        opset_version = 17, 
-        export_params = True, 
-        do_constant_folding = True,
-        dynamic_axes = {
-            "input_ids": { 1: "token_num" },
-            "attention_mask": { 1: "token_num" },
-            "token_type_ids": { 1: "token_num" },
-            "position_ids": { 1: "token_num" },
-            "text_self_attention_masks": { 1: "token_num", 2: "token_num" }
-        },
-    )
-
-    print("export onnx ok!")
-
-    onnx_model = onnx.load(onnx_file)
-    onnx.checker.check_model(onnx_model)
-    print("check model ok!")
-
-def inference(model):
-    image = cv2.imread('asset/cat_dog.jpeg')
-    processed_image = preprocess_image(image).unsqueeze(0)
-    caption = preprocess_caption("cat. dog")
-    tokenized = model.tokenizer(caption, padding="longest", return_tensors="pt")
-    box_threshold = torch.tensor(0.35, dtype=torch.float32)
-    text_threshold = torch.tensor(0.25, dtype=torch.float32)
-
-    specical_tokens = model.specical_tokens
-    (
-        text_self_attention_masks,
-        position_ids,
-        _,
-    ) = generate_masks_with_special_tokens_and_transfer_map(
-        tokenized, specical_tokens, model.tokenizer
-    )
-
-    outputs = model(processed_image, 
-                    tokenized["input_ids"], 
-                    tokenized["attention_mask"], 
-                    tokenized["token_type_ids"], 
-                    position_ids, 
-                    text_self_attention_masks, 
-                    box_threshold, 
-                    text_threshold)
-
-    prediction_logits = outputs[0] 
-    prediction_boxes = outputs[1] 
-    prediction_masks = outputs[2] 
-
-    input_ids = tokenized["input_ids"][0].tolist()
-    phrases = []
-    for mask in prediction_masks[0]:
-        prediction_token_ids = [input_ids[i] for i in mask.nonzero(as_tuple=True)[0].tolist()]
-        phrases.append(model.tokenizer.decode(prediction_token_ids).replace('.', ''))
-
-    with torch.no_grad():
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        image = annotate(image, prediction_boxes[0], prediction_logits[0], phrases)
-
-    cv2.imshow("image", image)
-    cv2.waitKey()
-    cv2.destroyAllWindows()
-
-def inference_onnx(model, output_dir):
-    onnx_file = output_dir + "/" + "gdino.onnx"
-    session = ort.InferenceSession(onnx_file)
-
+def inference_decoder_onnx(model, output):
     image = cv2.imread('asset/1.jpg')
     processed_image = preprocess_image(image).unsqueeze(0)
-    caption = preprocess_caption("watermark")
-    tokenized = model.tokenizer(caption, padding="longest", return_tensors="pt")
+
+    caption = "watermark. glasses"
+
+    tokenized, last_hidden_state = inference_encoder_onnx(model, output, caption)
+
+    print(tokenized)
+    print(last_hidden_state)
+
+    onnx_file = output + "/" + "gdino.decoder.onnx"
+    session = ort.InferenceSession(onnx_file)
+
     box_threshold = torch.tensor(0.35, dtype=torch.float32)
     text_threshold = torch.tensor(0.25, dtype=torch.float32)
 
-    specical_tokens = model.specical_tokens
-    (
-        text_self_attention_masks,
-        position_ids,
-        _,
-    ) = generate_masks_with_special_tokens_and_transfer_map(
-        tokenized, specical_tokens, model.tokenizer
-    )
-
-    max_text_len = model.max_text_len
-    if text_self_attention_masks.shape[1] > max_text_len:
-        text_self_attention_masks = text_self_attention_masks[
-            :, : max_text_len, : max_text_len
-        ]
-        position_ids = position_ids[:, : max_text_len]
-        tokenized["input_ids"] = tokenized["input_ids"][:, : max_text_len]
-        tokenized["attention_mask"] = tokenized["attention_mask"][:, : max_text_len]
-        tokenized["token_type_ids"] = tokenized["token_type_ids"][:, : max_text_len]
-
-    outputs = session.run(None, {
-        "image": processed_image.numpy().astype(np.float32) ,
-        "input_ids": tokenized["input_ids"].numpy().astype(np.int32) , 
-        "attention_mask": tokenized["attention_mask"].numpy().astype(np.uint8) , 
-        "token_type_ids": tokenized["token_type_ids"].numpy().astype(np.int32) , 
-        "position_ids": position_ids.numpy().astype(np.int32) , 
-        "text_self_attention_masks": text_self_attention_masks.numpy().astype(np.bool) , 
-        "box_threshold": box_threshold.numpy().astype(np.float32) ,
+    decode_outputs = session.run(None, {
+        "image": processed_image.numpy().astype(np.float32),
+        "last_hidden_state": last_hidden_state.numpy().astype(np.float32), 
+        "attention_mask": tokenized["attention_mask"].numpy().astype(np.uint8), 
+        "position_ids": tokenized["position_ids"].numpy().astype(np.int32), 
+        "text_self_attention_masks": tokenized["text_self_attention_masks"].numpy().astype(np.uint8), 
+        "box_threshold": box_threshold.numpy().astype(np.float32),
         "text_threshold": text_threshold.numpy().astype(np.float32) 
     })
 
-    prediction_logits = torch.from_numpy(outputs[0]) 
-    prediction_boxes = torch.from_numpy(outputs[1]) 
-    prediction_masks = torch.from_numpy(outputs[2]) 
+    prediction_logits = torch.from_numpy(decode_outputs[0]) 
+    prediction_boxes = torch.from_numpy(decode_outputs[1]) 
+    prediction_masks = torch.from_numpy(decode_outputs[2]) 
 
     input_ids = tokenized["input_ids"][0].tolist()
     phrases = []
@@ -224,9 +245,9 @@ def inference_onnx(model, output_dir):
     cv2.destroyAllWindows()
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser("Export Grounding DINO Model to IR", add_help=True)
-    parser.add_argument("--test", "-t", help="test onnx model", action="store_true")
-    parser.add_argument("--orig", "-n", help="test model", action="store_true")
+    parser = argparse.ArgumentParser("Export Grounding DINO Model to ONNX", add_help=True)
+    parser.add_argument("--encode", "-e", help="test encoder.onnx model", action="store_true")
+    parser.add_argument("--decode", "-d", help="test decoder.onnx model", action="store_true")
     parser.add_argument("--config_file", "-c", type=str, required=True, help="path to config file")
     parser.add_argument(
         "--checkpoint_path", "-p", type=str, required=True, help="path to checkpoint file"
@@ -245,11 +266,17 @@ if __name__ == "__main__":
     # make dir
     os.makedirs(output_dir, exist_ok=True)
 
-    model = Model(config_file, checkpoint_path, device='cpu')
+    source_model = load_model(model_config_path = config_file, 
+                              model_checkpoint_path = checkpoint_path, 
+                              device = "cpu").to("cpu")
 
-    if args.test:
-        inference_onnx(model, output_dir)
-    elif args.orig:
-        inference(model)
+    encoder = Encoder(source_model)
+    decoder = Decoder(source_model)
+
+    if args.encode:
+        inference_encoder_onnx(encoder, output_dir)
+    elif args.decode:
+        inference_decoder_onnx(decoder, output_dir)
     else:
-        export_onnx(model, output_dir)
+        export_encoder(encoder, output_dir)
+        export_decoder(decoder, output_dir, encoder)
